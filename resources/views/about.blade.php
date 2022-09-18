501cb45e6a0d8a078932e81f89dc48eabd2124cf
Gazi Akter <gaziakterpro@gmail.com>
2022-09-18 19:30:09 +0600

    Rout and controller

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About page</title>
+</head>
+<body>
+    <h1>Welcome to about page</h1>
+    <p>This is about page. Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.</p>
+</body>
+</html>
